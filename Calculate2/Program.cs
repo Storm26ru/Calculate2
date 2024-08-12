4561cebc4f20022b334b30254984235854dc0e07
Storm <Storm@STORMSSD>
2024-08-13 00:47:14 +0300

    done

--- Calculate2/Program.cs
+++ Calculate2/Program.cs
@@ -11,20 +11,47 @@ namespace Calculate2
 		static string expression;
 		static void Main(string[] args)
 		{
-			expression = "22 +33*77*55-50/11";
-			Console.WriteLine(Calculate(expression));
+			expression = "(2+3*(5+1)-5)+1"; //"(22+33)*77*(55-50/11)";
+			//Explore(expression);
+			Console.WriteLine(expression);
+			Console.WriteLine(Calculate(Explore(expression)));
 		}
-		static void Explore (string expression)
+		static string Explore (string expression)
         {
 			for(int i =0; i<expression.Length; i++)
             {
+
 				if (expression[i] == '(')
 				{
-					for(int j = i+1; j<expression.Length;j++)
-						if(expression[j] ==')') expression = expression.Replace(expression.Substring(i,j-i+1))
+						Console.WriteLine(expression);
+					for (int j = i + 1; j < expression.Length; j++)
+					{
+						if (expression[j] == '(')
+						{
+							expression = expression.Replace(expression.Substring(j, expression.Length - j),
+							Explore(expression.Substring(j, expression.Length - j)));
+							Console.WriteLine(expression);
+							break;
+						}
+						if (expression[j] == ')')
+						{
+							expression = expression.Replace(expression.Substring(i, j - i + 1),
+															Calculate(expression.Substring(i + 1, j - i - 1)).ToString());
+							Console.WriteLine(expression);
+							break;
+						}
+					}
 				} 
 
+				if (expression[i]==')')
+				{
+					expression = expression.Replace(expression.Substring(0, i+1),
+															Calculate(expression.Substring(0, i)).ToString());
+							Console.WriteLine(expression);
+					break;
+				}
             }
+			return expression;
         }
 		static double Calculate(string expression)
 		{
@@ -34,9 +61,9 @@ namespace Calculate2
 			for (int i = 0; i < s_operands.Length; i++) d_operands[i] = Convert.ToDouble(s_operands[i]);
 			//char[] operators = expression.Where(item => "+-*/".Contains(item)).ToArray();
 			string[] operators = (expression.Split('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',')).Where(item=>item!="").ToArray();
-			Print(d_operands);
+			//Print(d_operands);
 			//Console.WriteLine();
-			Print(operators);
+			//Print(operators);
 			for(int i = 0; i<operators.Length;i++)
 			{
 				while (operators[i] == "*" || operators[i] == "/")
